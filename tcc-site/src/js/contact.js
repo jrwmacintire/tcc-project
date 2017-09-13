3cee1b15bdc29318eff9a36eb658628600fdf6a8
John MacIntire <jrwmacintire@gmail.com>
2017-09-13 13:36:37 -0700

    kjhl;jkh
    kjhlkjh

---
+++ tcc-site/src/js/contact.js
@@ -0,0 +1,23 @@
+$.ajax({
+    url: "/rest/contact/",
+    type: "POST",
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    data: JSON.stringify({
+        name: name,
+        email: email,
+        phone: phone,
+        message: message
+    }),
+    cache: false,
+    success: function (response) {
+        if (response.errorMessage) {
+            this.error(response.errorMessage);
+            return;
+        }
+        // process success message
+    },
+    error: function (msg) {
+        // process errors
+    }
+});
